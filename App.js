f6e64147f2b3323dcb750499568d9dd6f756b9ec
NtsakoMphongola <105928245+NtsakoMphongola@users.noreply.github.com>
2022-08-19 16:16:22 +0200

    updates

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Button, Image } from 'react-native';
 import { useEffect, useRef, useState } from 'react';
 import { Camera } from 'expo-camera';
 import { shareAsync } from 'expo-sharing';
 import * as MediaLibrary from 'expo-media-library';
 
+let apiKey = 'YOUR_API_KEY';
+
+import * as Location from 'expo-location';
+
 export default function App() {
+
   let cameraRef = useRef();
   const [CameraPermission, setCameraPermission] = useState();
   const [MediaLPermission, setMediaLibraryPermission] = useState();
   const [photo, setPhoto] = useState();
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [address, setAddress] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -32,7 +40,34 @@ export default function App() {
     };
     let newPhoto = await cameraRef.current.takePictureAsync(options);
     setPhoto(newPhoto);
+    
+  let { status } = await Location.requestPermissionsAsync();
+  if (status !== 'granted') {
+    setErrorMsg('Permission to access location was denied');
+  }
+
+  Location.setGoogleApiKey(apiKey);
+
+  console.log(status);
+
+  let { coords } = await Location.getCurrentPositionAsync();
+
+  setLocation(coords);
+
+  console.log(coords);
+
+  if (coords) {
+    let { longitude, latitude } = coords;
+
+    let regionName = await Location.reverseGeocodeAsync({
+      longitude,
+      latitude,
+    });
+    setAddress(regionName[0]);
+    console.log(regionName, 'nothing');
+  }
   };
+
 
   if (photo) {
     let sharePic = () => {
@@ -44,13 +79,19 @@ export default function App() {
     let savePhoto = () => {
       MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
         setPhoto(undefined);
-        alert('Image saved, take another')
       });
     };
 
     return (
       <SafeAreaView style={styles.container}>
         <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} />
+        <Text style={styles.big}>
+        {!location
+          ? 'Waiting'
+          : `${JSON.stringify(address?.['subregion'])}
+             ${JSON.stringify(address?.['city'])}`
+             }
+      </Text>
         {MediaLPermission ? <Button title="Save" onPress={savePhoto} color='green' /> : undefined}
         <Button title="Share" onPress={sharePic} color='gray' />
         <Button title="Discard" onPress={() => setPhoto(undefined)} color='red' />
@@ -61,7 +102,7 @@ export default function App() {
   return (
     <Camera style={styles.container} ref={cameraRef}>
       <View style={styles.btncontainer}>
-        <Button title="Take Pic" onPress={takePic} />
+        <TouchableOpacity title="Take Pic" onPress={takePic} />
       </View>
       <StatusBar style="auto" />
     </Camera>
